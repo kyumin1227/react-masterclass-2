164ac409c101335ef2cfff1b7c743c92e14a5c16
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-07-02 23:33:14 +0900

    Props in typescript 2 (optional, default)

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 //Container의 props의 타입을 지정 (object를 설명)
 interface ContainerProps {
     bgColor: string;
+    borderColor: string;
 }
 
 // 타입을 지정한 인터페이스와 연결 <ContainerProps>
@@ -11,17 +12,21 @@ const Container = styled.div<ContainerProps>`
     height: 200px;
     background-color: ${(props) => props.bgColor};
     border-radius: 100px;
+    border: 1px solid ${(props) => props.borderColor};
 `;
 
 // Circle의 Props의 타입을 지정 (object를 설명)
 interface CircleProps {
-    bgColor: string;
+    bgColor: string;    
+    borderColor?: string;   // option으로 설정 ?
+    text?: string;
 }
 
 // 타입을 지정한 인터페이스와 연결 : CircleProps
-function Circle({bgColor}: CircleProps) {
-    return <Container bgColor={bgColor}>
-        
+function Circle({ bgColor, borderColor, text = "default text" /* undefined일 경우 초기값 지정 방법 2 (es6) */ }: CircleProps) {
+    // undefined일 경우 초기값 지정 방법 1 (typescript) ??
+    return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+        {text}
     </Container>;
 }
 
