34aab07cceb659d76a6008284be1559a28990801
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-08-01 14:28:58 +0900

    ApexChart 1

--- src/router/Chart.tsx
+++ src/router/Chart.tsx
@@ -1,6 +1,19 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
 import { useOutletContext } from "react-router-dom";
+import ApexChart from "react-apexcharts";
+
+
+interface IHistorical {
+    time_open: number;
+    time_close: number;
+    open: string;
+    high: string;
+    low: string;
+    close: string;
+    volume: string;
+    market_cap: number;
+}
 
 interface IChartProps {
     coinId: string;
@@ -12,9 +25,31 @@ function Chart() {
     console.log("chart", coinId);
     
 
-    const { isLoading, data } = useQuery(["chart", coinId], () => fetchCoinHistory(coinId));
+    const { isLoading, data } = useQuery<IHistorical[]>(["chart", coinId], () => fetchCoinHistory(coinId));
     
-    return <h1>Chart</h1>
+    return <div>{isLoading ? "Loading..." : <ApexChart options={{
+        theme: {
+            mode: "dark",
+        },
+        chart: {
+            height: 500,
+            width: 500,
+            toolbar: {
+                show: false,
+            },
+            background: "transparent",
+        },
+        grid:{show: false},
+        stroke: {
+            curve: "smooth",
+            width: 3,
+        }
+    }} type="line" series={[
+        {
+            name: "Price",
+            data: data?.map((price) => Number(price.close))??[],
+        }
+    ]} />}</div>
 }
 
 export default Chart;
