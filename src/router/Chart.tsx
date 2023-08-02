559d1515aac39261ef0bcf3e08caf6f9f0d39337
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-08-02 12:42:27 +0900

    ApexChart 2

--- src/router/Chart.tsx
+++ src/router/Chart.tsx
@@ -21,6 +21,8 @@ interface IChartProps {
 
 function Chart() {
 
+    const example = new Date(0).toISOString()
+
     const {coinId} = useOutletContext<IChartProps>();
     console.log("chart", coinId);
     
@@ -41,15 +43,35 @@ function Chart() {
         },
         grid:{show: false},
         stroke: {
-            curve: "smooth",
-            width: 3,
+            curve: "smooth", // 그래프의 커브 굴곡
+            width: 3, // 그래프 굵기
+        },
+        yaxis: {
+            show: false, // y축 단위 표시 여부
+        },
+        xaxis: {
+            axisBorder: { show: false }, // x축의 테두리 표시 여부
+            axisTicks: { show: false }, // x축의 격자 표시 여부
+            labels: { show: false }, // x축 단위 표시 여부
+            categories: data?.map((price) => (new Date(price.time_close).toUTCString())), // x축의 값을 설정
+            type: "datetime",
+        },
+        fill: {
+            type: "gradient",
+            gradient: {gradientToColors: ["blue"], stops: [0, 100]},
+        },
+        colors: ["red"],
+        tooltip: {
+            y: {
+                formatter: (value) => `$ ${value.toFixed(2)}` // y축의 값을 해당 형식으로 포맷 (앞에 $ 붙이고 숫자는 소수점 둘째자리까지 표시)
+            }
         }
     }} type="line" series={[
         {
             name: "Price",
             data: data?.map((price) => Number(price.close))??[],
         }
-    ]} />}</div>
+        ]} />}</div>
 }
 
 export default Chart;
