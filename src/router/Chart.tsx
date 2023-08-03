b94d5e7dacf8774a499b77b7a45ec50a729a5efa
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-08-03 13:50:50 +0900

    Challenge : Chart type change line -> candlestick

--- src/router/Chart.tsx
+++ src/router/Chart.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
 import { useOutletContext } from "react-router-dom";
 import ApexChart from "react-apexcharts";
+import { useState } from "react";
 
 
 interface IHistorical {
@@ -28,12 +29,24 @@ function Chart() {
     
 
     const { isLoading, data } = useQuery<IHistorical[]>(["chart", coinId], () => fetchCoinHistory(coinId));
+
+    const exceptData = data ?? [];
+    const chartData = exceptData?.map((i) => {
+        return {
+            x: i.time_close,
+            y: [Number(i.open), Number(i.high), Number(i.low), Number(i.close)],
+        };
+    });
+
+    console.log(chartData);
+    
     
     return <div>{isLoading ? "Loading..." : <ApexChart options={{
         theme: {
             mode: "dark",
         },
         chart: {
+            type: "candlestick",
             height: 500,
             width: 500,
             toolbar: {
@@ -41,35 +54,41 @@ function Chart() {
             },
             background: "transparent",
         },
-        grid:{show: false},
-        stroke: {
-            curve: "smooth", // 그래프의 커브 굴곡
-            width: 3, // 그래프 굵기
-        },
-        yaxis: {
-            show: false, // y축 단위 표시 여부
-        },
         xaxis: {
-            axisBorder: { show: false }, // x축의 테두리 표시 여부
-            axisTicks: { show: false }, // x축의 격자 표시 여부
-            labels: { show: false }, // x축 단위 표시 여부
-            categories: data?.map((price) => (new Date(price.time_close).toUTCString())), // x축의 값을 설정
             type: "datetime",
         },
-        fill: {
-            type: "gradient",
-            gradient: {gradientToColors: ["blue"], stops: [0, 100]},
-        },
-        colors: ["red"],
-        tooltip: {
-            y: {
-                formatter: (value) => `$ ${value.toFixed(2)}` // y축의 값을 해당 형식으로 포맷 (앞에 $ 붙이고 숫자는 소수점 둘째자리까지 표시)
-            }
-        }
-    }} type="line" series={[
+        // grid:{show: false},
+        // stroke: {
+        //     curve: "smooth", // 그래프의 커브 굴곡
+        //     width: 3, // 그래프 굵기
+        // },
+        // yaxis: {
+        //     show: false, // y축 단위 표시 여부
+        // },
+        // xaxis: {
+        //     axisBorder: { show: false }, // x축의 테두리 표시 여부
+        //     axisTicks: { show: false }, // x축의 격자 표시 여부
+        //     labels: { show: false }, // x축 단위 표시 여부
+        //     categories: data?.map((price) => (new Date(price.time_close).toUTCString())), // x축의 값을 설정
+        //     type: "datetime",
+        // },
+        // fill: {
+        //     type: "gradient",
+        //     gradient: {gradientToColors: ["blue"], stops: [0, 100]},
+        // },
+        // colors: ["red"],
+        // tooltip: {
+        //     y: {
+        //         formatter: (value) => `$ ${value.toFixed(2)}` // y축의 값을 해당 형식으로 포맷 (앞에 $ 붙이고 숫자는 소수점 둘째자리까지 표시)
+        //     }
+        // }
+    }} type="candlestick" series={[
         {
-            name: "Price",
-            data: data?.map((price) => Number(price.close))??[],
+            data: chartData.map((i) => {
+                return {
+                x: new Date(i.x),
+                y: i.y,
+            }})??[],
         }
         ]} />}</div>
 }
